084b3b6ccb69807a4d9db8ed490bf9b7f8713e3a
Soohwan Park <vividswan@naver.com>
2020-10-06 00:54:49 +0900

    Create 10950_A+B-3.cpp

---
+++ BOJ/구현/10950_A+B-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int t;
+    cin >> t;
+    while(t--){
+        int num1, num2;
+        cin >> num1 >> num2;
+        cout << num1+num2 << '\n';
+    }
+    return 0;
+}
