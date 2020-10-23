f72ba7488d3968dbe8dcc77bf0409a35e6ac8255
Soohwan Park <vividswan@naver.com>
2020-10-23 16:36:00 +0900

    Update 11729_하노이 탑 이동 순서.cpp

--- BOJ/탐색/재귀(Recursion)/11729_하노이 탑 이동 순서.cpp
+++ BOJ/탐색/재귀(Recursion)/11729_하노이 탑 이동 순서.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
+#include <queue>
 using namespace std;
-void hanoi(int now, int go, int ed);
-long double cnt(int now);
-int main(void) {
-	int now;
-	cin >> now;
-	printf("%.0Lf\n", cnt(now));
-	if (now <= 20) hanoi(now, 1, 3);
-	return 0;
+queue <pair<int,int>> q;
+void recursion(int idx, int st, int ed);
+int main() {
+    int n;
+    cin >> n;
+    recursion(n, 1, 3);
+    cout << q.size() << '\n';
+    while (!q.empty()) {
+        cout << q.front().first << ' ' << q.front().second << '\n';
+        q.pop();
+    }
+    return 0;
 }
-void hanoi(int now, int go, int ed) {
-	if (now > 1) {
-		int mid;
-		if (go == 1 && ed == 2) mid = 3;
-		else if (go == 1 && ed == 3) mid = 2;
-		else if (go == 2 && ed == 3) mid = 1;
-		else if (go == 2 && ed == 1) mid = 3;
-		else if (go == 3 && ed == 1) mid = 2;
-		else if (go == 3 && ed == 2) mid = 1;
-		hanoi(now - 1, go, mid);
-		cout << go << ' ' << ed << "\n";
-		hanoi(now - 1, mid, ed);
-	}
-	else if (now == 1) {
-		cout << go << ' ' << ed << "\n";
-		return;
-	}
-
-}
-long double cnt(int now) {
-	long double res = 1;
-	for (int i = 2; i <= now; i++) {
-		res = 2 * res + 1;
-	}
-	return res;
+void recursion(int idx, int st, int ed){
+    if(idx==1){
+        q.push({st,ed});
+        return;
+    }
+    int mid = 6-st-ed;
+    recursion(idx-1,st,mid);
+    q.push({st,ed});
+    recursion(idx-1,mid,ed);
 }
