dad1daba70aa907a88df8bba17d81192c30af884
vividswan <vividswan@naver.com>
2020-12-30 08:15:44 +0900

    Create 18406_럭키 스트레이트.java

---
+++ BOJ/구현/18406_럭키 스트레이트.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 럭키 스트레이트 {
+
+    public static boolean chkFunc(String str){
+        int tot1 = 0;
+        for(int i=0;i<str.length()/2;i++){
+            char c = str.charAt(i);
+            tot1 += c-'0';
+        }
+
+        int tot2 = 0;
+        for(int i=str.length()/2;i<str.length();i++){
+            char c = str.charAt(i);
+            tot2+=c-'0';
+        }
+
+        if(tot1==tot2) return true;
+        else return false;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        if(chkFunc(str)) bw.write("LUCKY\n");
+        else bw.write("READY\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
