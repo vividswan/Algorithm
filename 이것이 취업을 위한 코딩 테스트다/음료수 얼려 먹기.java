7c0566df3e9dfb00e7f9126c250add27ac30f623
vividswan <vividswan@naver.com>
2021-03-11 13:28:00 +0900

    Update 음료수 얼려 먹기.java

--- 이것이 취업을 위한 코딩 테스트다/음료수 얼려 먹기.java
+++ 이것이 취업을 위한 코딩 테스트다/음료수 얼려 먹기.java
@@ -1,48 +1,50 @@
+import java.io.*;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
-
-public class 음료수 얼려 먹기{
-
-    public static int map[][] = new int[1001][1001];
-    public static int dx[] = {0,0,-1,1};
-    public static int dy[] = {1,-1,0,0};
-    public static int vis[][] = new int[1001][1001];
-    public static int n,m;
-
+public class 음료수 얼려 먹기 {
+    public static int n,m,cnt;
+    public static int[][] map;
+    public static boolean[][] vis;
+    public static int[] dx = {0,0,1,-1};
+    public static int[] dy = {1,-1,0,0};
     public static void dfs(int x, int y){
-        for(int i=0;i<4;i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(nx<0||ny<0||nx>=n||ny>=m) continue;
-            if(map[nx][ny]==1) continue;;
-            if(vis[nx][ny]==1) continue;
-            vis[nx][ny] = 1;
+        vis[x][y] = true;
+        for(int i=0; i<4;i ++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx<1||ny<1||nx>n||ny>m) continue;
+            if(map[nx][ny]==1) continue;
+            if(vis[nx][ny]) continue;
             dfs(nx,ny);
         }
     }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        m = scanner.nextInt();
-        scanner.nextLine();
-
-        for(int i=0;i<n;i++){
-            String str = scanner.nextLine();
-            for(int j=0;j<m;j++){
-                map[i][j] = str.charAt(j)-'0';
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n+1][m+1];
+        vis = new boolean[n+1][m+1];
+        for(int i=1; i<=n; i++){
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            for(int j=1; j<=m; j++){
+                map[i][j] = str.charAt(j-1) - '0';
             }
         }
-
-        int result = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]==1 || map[i][j]==1) continue;
-                result++;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(vis[i][j]) continue;
+                if(map[i][j]==1) continue;
+                cnt++;
                 dfs(i,j);
             }
         }
-        System.out.println(result);
-
+        bw.write(String.valueOf(cnt)+"\n");
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
