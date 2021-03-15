f617f64d631fe372da59b6aa8e50e5c3a6d2f5a5
vividswan <vividswan@naver.com>
2021-03-16 08:37:37 +0900

    Update 1로 만들기.java

--- 이것이 취업을 위한 코딩 테스트다/1로 만들기.java
+++ 이것이 취업을 위한 코딩 테스트다/1로 만들기.java
@@ -1,22 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
-public class 1로 만들기{
-    public static int[] dp;
+public class 1로 만들기 {
     public static int n;
+    public static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        dp = new int[n+1];
-        dp[1] = 0;
-        for(int i=2;i<=n;i++){
-            int result = dp[i-1]+1;
-            if(i%2==0) result = Math.min(result,dp[i/2]+1);
-            if(i%3==0) result = Math.min(result,dp[i/3]+1);
-            if(i%5==0) result = Math.min(result,dp[i/5]+1);
-            dp[i] = result;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n+1];
+        for(int i=2; i<=n; i++) arr[i] = n+1;
+        for(int i=1; i<=n; i++){
+            if(i*5<=n) arr[i*5] = Math.min(arr[i]+1, arr[i*5]);
+            if(i*3<=n) arr[i*3] = Math.min(arr[i]+1, arr[i*3]);
+            if(i*2<=n) arr[i*2] = Math.min(arr[i]+1, arr[i*2]);
+            if(i+1<=n) arr[i+1] = Math.min(arr[i]+1, arr[i+1]);
         }
-        System.out.println(dp[n]);
+        bw.write(String.valueOf(arr[n]));
+        bw.flush();
+        br.close();
+        bw.close();
     }
-
 }
