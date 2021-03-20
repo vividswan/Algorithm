ae92eab7e0a1f0327e128d403aa972be9dd343f4
vividswan <vividswan@naver.com>
2021-03-20 16:37:25 +0900

    Update 효율적인 화폐 구성.java

--- 이것이 취업을 위한 코딩 테스트다/효율적인 화폐 구성.java
+++ 이것이 취업을 위한 코딩 테스트다/효율적인 화폐 구성.java
@@ -1,31 +1,43 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class 효율적인 화폐 구성 {
+    public static int n,m;
+    public static int[] arr;
+    public static int[] dp;
 
-    public static final int MAX = 10001;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-
-        int[] list = new int[n];
-
-        for(int i=0;i<n;i++){
-            list[i] = scanner.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        dp = new int[m+1];
+        for(int i=1; i<=m; i++) dp[i] = m+1;
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int[] dp = new int[m+1];
-        for(int i=1;i<=m;i++) dp[i] = MAX;
-
-        for(int i=0;i<n;i++){
-            for(int j=1;j<=m;j++){
-                if(j-list[i]<0) continue;
-                dp[j] = Math.min(dp[j],dp[j-list[i]]+1);
+        Arrays.sort(arr);
+        for(int i=0; i<n; i++){
+            int value = arr[i];
+            for(int j=1; j<=m; j++){
+                int now = j-value;
+                if(now >= 0){
+                    if(dp[now] != m+1){
+                        dp[j] = Math.min(dp[now]+1,dp[j]);
+                    }
+                }
             }
         }
-        if(dp[m]==MAX) System.out.println(-1);
-        else System.out.println(dp[m]);
+
+        if(dp[m]==m+1) bw.write("-1\n");
+        else bw.write(dp[m]+"\n");
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
