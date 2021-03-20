9f006cf772139c241afb9066aa9b2b1e22a0ff2d
vividswan <vividswan@naver.com>
2021-03-21 00:14:55 +0900

    Update 바닥 공사.java

--- 이것이 취업을 위한 코딩 테스트다/바닥 공사.java
+++ 이것이 취업을 위한 코딩 테스트다/바닥 공사.java
@@ -1,24 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
-public class 바닥 공사{
-
-    public static final int  MOD =  796796;
+public class 바닥공사 {
+    public static int n;
     public static int[] dp;
+    public static final int MOD = 796796;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
         dp = new int[n+1];
-        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 3;
-        for(int i = 3;i<=n;i++){
-            dp[i] += dp[i-1] % MOD;
-            dp[i] += dp[i-2] * 2 % MOD;
-            dp[i] %= MOD;
+        for(int i=3; i<=n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % MOD;
         }
-        System.out.println(dp[n]);
-
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+        br.close();
+        bw.close();
     }
-
 }
