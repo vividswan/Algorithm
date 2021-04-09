2322c32ab5d3e29587908a6a183f5112240eb79a
vividswan <vividswan@naver.com>
2021-04-09 11:03:23 +0900

    Update 기둥과 보 설치.java

--- Programmers/Level 3/기둥과 보 설치.java
+++ Programmers/Level 3/기둥과 보 설치.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 class Node implements Comparable<Node>{
     private int x;
     private int y;
-    private  int article;
+    private int a;
 
     public int getX() {
         return x;
@@ -14,125 +14,108 @@ class Node implements Comparable<Node>{
         return y;
     }
 
-    public int getArticle() {
-        return article;
+    public int getA() {
+        return a;
     }
 
-    public Node(int x, int y, int article){
+    public Node(int x, int y, int a){
         this.x = x;
         this.y = y;
-        this.article = article;
+        this.a = a;
     }
 
     @Override
     public int compareTo(Node other) {
-        if(this.x==other.x && this.y == other.y) return Integer.compare(this.article,other.article);
-        else if(this.x == other.x) return Integer.compare(this.y, other.y);
-        else return Integer.compare(this.x, other.x);
+        if(this.x != other.x) return Integer.compare(this.x,other.x);
+        else if(this.y != other.y) return Integer.compare(this.y,other.y);
+        else return Integer.compare(this.a,other.a);
     }
 }
 
 class Solution {
 
-    public static boolean chkPossible(){
-        for(int i=0;i<result.size();i++){
-            ArrayList<Integer> nowNode = result.get(i);
-            int x = nowNode.get(0);
-            int y = nowNode.get(1);
-            int a = nowNode.get(2);
+    public static ArrayList<Node> list = new ArrayList<>();
 
-            if(a==0){
-                boolean chk = false;
+    public boolean search(ArrayList<Node> temp){
+        for(int i=0; i<temp.size();i++){
+            Node node = temp.get(i);
+            boolean chk = false;
+            int  x = node.getX();
+            int y = node.getY();
+            int a = node.getA();
+
+            if(a==0) {
                 if(y==0) chk = true;
-                for(int j=0; j<result.size();j++){
-                    ArrayList<Integer> otherNode = result.get(j);
-                    int otherX = otherNode.get(0);
-                    int otherY = otherNode.get(1);
-                    int otherA = otherNode.get(2);
-                    if(otherA==0){
-                        if(otherX==x && otherY==y-1) chk = true;
-                    }
-                    else {
-                        if(otherX==x && otherY==y) chk = true;
-                        else if(otherX==x-1 && otherY==y) chk = true;
-                    }
+                for(int j=0; j<temp.size();j++){
+                    Node side = temp.get(j);
+                    if(side.getA()==0 && side.getY()==y-1 && side.getX() == x) chk=true;
+                    if(side.getA()==1 && side.getY() ==y && side.getX() == x-1) chk = true;
+                    if(side.getA()==1 && side.getY() ==y && side.getX() == x) chk = true;
                 }
-                if(chk==false) return false;
+                if(!chk) return false;
             }
-            else if(a==1){
-                boolean chk = false;
-                boolean le = false;
-                boolean ri = false;
-                if(y==0) chk = true;
-                for(int j=0; j<result.size();j++){
-                    ArrayList<Integer> otherNode = result.get(j);
-                    int otherX = otherNode.get(0);
-                    int otherY = otherNode.get(1);
-                    int otherA = otherNode.get(2);
-                    if(otherA==0){
-                        if(otherX==x && otherY == y-1) chk = true;
-                        if(otherX==x+1 && otherY == y-1) chk = true;
-                    }
-                    else {
-                        if(otherX==x-1 && otherY == y) le = true;
-                        if(otherX==x+1 && otherY == y) ri = true;
-                    }
+            else {
+                boolean right = false;
+                boolean left = false;
+                for(int j=0; j<temp.size(); j++){
+                    Node side = temp.get(j);
+                    if(side.getA()==0 && side.getY()==y-1 && (side.getX() == x || side.getX() == x+1)) chk = true;
+                    if(side.getA()==1 && side.getY()==y && side.getX()==x+1) right = true;
+                    if(side.getA()==1 && side.getY()==y && side.getX()==x-1) left = true;
                 }
-                if(le&&ri) chk = true;
-                if(chk==false) return false;
+                if(right && left) chk = true;
+                if(!chk) return false;
             }
+
         }
         return true;
     }
 
-    public static ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    public boolean isPossible(int x, int y, int a, int b){
+        ArrayList<Node> temp = new ArrayList<>();
+        for(int i=0; i<list.size();i++){
+            temp.add(list.get(i));
+        }
+        Node node = new Node(x,y,a);
+        if(b==1) temp.add(node);
+        else {
+            for(int i=0; i<temp.size();i++){
+                Node now = temp.get(i);
+                if(now.getX()==x && now.getY()==y && now.getA() == a) temp.remove(i);
+            }
+        }
+        return search(temp);
+    }
 
     public int[][] solution(int n, int[][] build_frame) {
-
-        for(int i=0;i<build_frame.length;i++){
+        for(int i=0; i < build_frame.length; i++){
             int x = build_frame[i][0];
             int y = build_frame[i][1];
             int a = build_frame[i][2];
             int b = build_frame[i][3];
-
-            if(b==1){
-                ArrayList<Integer> nowNode = new ArrayList<>();
-                nowNode.add(x);
-                nowNode.add(y);
-                nowNode.add(a);
-                result.add(nowNode);
-                if(!chkPossible()) result.remove(result.size()-1);
-            }
-            else if(b==0){
-                int idx = 0;
-                for(int j=0;j<result.size();j++){
-                    if(x==result.get(j).get(0)&&y==result.get(j).get(1)&&a==result.get(j).get(2)){
-                        idx = j;
-                        break;
+            if(isPossible(x,y,a,b)){
+                Node node = new Node(x,y,a);
+                if(b==1) list.add(node);
+                else {
+                    for(int j=0; j<list.size();j++){
+                        Node now = list.get(j);
+                        if(now.getX()==x && now.getY()==y && now.getA() == a) list.remove(j);
                     }
                 }
-                ArrayList<Integer> nowNode = result.get(idx);
-                result.remove(idx);
-                if(!chkPossible()) result.add(nowNode);
             }
-
-
-        }
-
-        ArrayList<Node> list = new ArrayList<>();
-
-        for(int i = 0; i<result.size();i++){
-            list.add(new Node(result.get(i).get(0),result.get(i).get(1),result.get(i).get(2)));
         }
 
         Collections.sort(list);
 
+
         int[][] answer = new int[list.size()][3];
-        for(int i=0;i<list.size();i++){
+        for(int i=0; i<list.size();i++){
             answer[i][0] = list.get(i).getX();
             answer[i][1] = list.get(i).getY();
-            answer[i][2] = list.get(i).getArticle();
+            answer[i][2] = list.get(i).getA();
         }
+
+
         return answer;
     }
 }
