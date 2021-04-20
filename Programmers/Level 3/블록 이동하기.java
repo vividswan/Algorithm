42b108b3fce468761ad3ac9da7958b1a5450fe9c
vividswan <vividswan@naver.com>
2021-04-20 17:54:45 +0900

    Update 블록 이동하기.java

--- Programmers/Level 3/블록 이동하기.java
+++ Programmers/Level 3/블록 이동하기.java
@@ -1,15 +1,15 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Position{
-
+class Node{
     private int x;
     private int y;
+    private int dir;
 
-    public Position(int x, int y){
+    public Node(int dir, int x, int y){
         this.x = x;
         this.y = y;
+        this.dir = dir;
     }
 
     public int getX() {
@@ -19,224 +19,112 @@ class Position{
     public int getY() {
         return y;
     }
-}
 
-class Robot{
-    private Position position1;
-    private Position position2;
-    private boolean isHorizon;
-    private int num;
-
-    public Robot(Position position1, Position position2, int num){
-        this.position1 = position1;
-        this.position2 = position2;
-        this.num = num;
-        if(position1.getX()== position2.getX()) this.isHorizon = true;
-        else this.isHorizon = false;
-    }
-
-    public Position getPosition1() {
-        return position1;
-    }
-
-    public Position getPosition2() {
-        return position2;
-    }
-
-    public boolean isHorizon() {
-        return isHorizon;
-    }
-
-    public int getNum() {
-        return num;
+    public int getDir() {
+        return dir;
     }
 }
 
 class Solution {
 
+    public static int[] dx = {0,0,1,-1};
+    public static int[] dy = {1,-1,0,0};
+    public static int[][][] vis;
     public static int n;
-    public static int[][] map;
-    public static  int resultVal = (int) 1e9;
-    public static int[][][][] vis;
-
-    public ArrayList<Robot> move(Robot robot){
-        Position position1 = robot.getPosition1();
-        Position position2 = robot.getPosition2();
-
-        int num = robot.getNum();
-        int x1 = position1.getX();
-        int y1 = position1.getY();
-        int x2 = position2.getX();
-        int y2 = position2.getY();
-
-        ArrayList<Robot> result = new ArrayList<>();
-
-        if(robot.isHorizon()){
-            if(x1-1>=0){
-                if(map[x1-1][y1]==0 && map[x2-1][y2]==0){
-                    if(vis[x1-1][y1][x2-1][y2]==0){
-                        result.add(new Robot(new Position(x1-1,y1), new Position(x2-1,y2),num+1));
-                        vis[x1-1][y1][x2-1][y2]=vis[x1][y1][x2][y2]+1;
-                    }
-                }
-            }
-            if(x1+1<n){
-                if(map[x1+1][y1]==0 && map[x2+1][y2]==0){
-                    if(vis[x1+1][y1][x2+1][y2]==0){
-                        result.add(new Robot(new Position(x1+1,y1), new Position(x2+1,y2),num+1));
-                        vis[x1+1][y1][x2+1][y2]=vis[x1][y1][x2][y2]+1;
-                    }
-                }
-            }
-            if(y2+1<n) {
-                if (map[x2][y2+1]==0) {
-                    if (vis[x2][y2][x2][y2 + 1] == 0) {
-                        result.add(new Robot(position2, new Position(x2, y2 + 1),num+1));
-                        vis[x2][y2][x2][y2+1] = vis[x1][y1][x2][y2]+1;
-                    }
-                }
-            }
-            if(y1-1>=0) {
-                if (map[x1][y1-1]==0) {
-                    if (vis[x1][y1 - 1][x1][y1] == 0) {
-                        result.add(new Robot(new Position(x1,y1-1),position1,num+1));
-                        vis[x1][y1 - 1][x1][y1] = vis[x1][y1][x2][y2]+1;
-                    }
-                }
-            }
-            if(x1-1>=0) {
-                if (map[x1-1][y1]==0) {
-                    if (vis[x1-1][y1 ][x1][y1] == 0) {
-                        if(map[x2-1][y2]==0){
-                            result.add(new Robot(new Position(x1-1,y1),position1,num+1));
-                            vis[x1-1][y1][x1][y1] = vis[x1][y1][x2][y2]+1;
-                        }
-                    }
-                }
-            }
-            if(x2-1>=0) {
-                if (map[x2-1][y2]==0) {
-                    if (vis[x2-1][y2 ][x2][y2] == 0) {
-                        if(map[x1-1][y1]==0){
-                            result.add(new Robot(new Position(x2-1,y2),position2,num+1));
-                            vis[x2-1][y2][x2][y2] = vis[x1][y1][x2][y2]+1;
-                        }
-                    }
-                }
-            }
-            if(x1+1<n) {
-                if (map[x1+1][y1]==0) {
-                    if (vis[x1][y1][x1+1][y1 ] == 0) {
-                        if(map[x1+1][y1+1]==0){
-                            result.add(new Robot(position1,new Position(x1+1,y1),num+1));
-                            vis[x1][y1][x1+1][y1] = vis[x1][y1][x2][y2]+1;
-                        }
-                    }
-                }
-            }
-            if(x2+1<n) {
-                if (map[x2+1][y2]==0) {
-                    if (vis[x2][y2][x2+1][y2 ] == 0) {
-                        if(map[x2+1][y2-1]==0){
-                            result.add(new Robot(position2,new Position(x2+1,y2),num+1));
-                            vis[x2][y2][x2+1][y2] = vis[x1][y1][x2][y2]+1;
-                        }
-                    }
-                }
-            }
-        }
-        else{
-            if(y1-1>=0){
-                if(map[x1][y1-1]==0 && map[x2][y2-1]==0){
-                    if(vis[x1][y1-1][x2][y2-1]==0){
-                        result.add(new Robot(new Position(x1,y1-1), new Position(x2,y2-1),num+1));
-                        vis[x1][y1-1][x2][y2-1]=vis[x1][y1][x2][y2]+1;
-                    }
-                }
-            }
-            if(y1+1<n){
-                if(map[x1][y1+1]==0 && map[x2][y2+1]==0){
-                    if(vis[x1][y1+1][x2][y2+1]==0){
-                        result.add(new Robot(new Position(x1,y1+1), new Position(x2,y2+1),num+1));
-                        vis[x1][y1+1][x2][y2+1]=vis[x1][y1][x2][y2]+1;
-                    }
-                }
-            }
-            if(x2+1<n){
-                if(map[x2+1][y2]==0){
-                    if(vis[x2][y2][x2+1][y2]==0){
-                        result.add(new Robot(position2,new Position(x2+1,y2),num+1));
-                        vis[x2][y2][x2+1][y2] = vis[x1][y1][x2][y2]+1;
-                    }
-                }
-            }
-            if(x1-1>=0){
-                if(map[x1-1][y1]==0){
-                    if(vis[x1-1][y1][x1][y1]==0){
-                        result.add(new Robot(new Position(x1-1,y1),position1,num+1));
-                        vis[x1-1][y1][x1][y1] = vis[x1][y1][x2][y2]+1;
-                    }
-                }
-            }
-            if(y1+1<n){
-                if(map[x1][y1+1]==0){
-                    if(vis[x1][y1][x1][y1+1]==0){
-                        if(map[x2][y2+1]==0){
-                            result.add(new Robot(position1,new Position(x1,y1+1),num+1));
-                            vis[x1][y1][x1][y1+1] = vis[x1][y1][x2][y2]+1;
-                        }
-                    }
-                }
-            }
-            if(y2+1<n){
-                if(map[x2][y2+1]==0){
-                    if(vis[x2][y2][x2][y2+1]==0){
-                        if(map[x1][y1+1]==0){
-                            result.add(new Robot(position2,new Position(x2,y2+1),num+1));
-                            vis[x2][y2][x2][y2+1] = vis[x1][y1][x2][y2]+1;
-                        }
-                    }
-                }
-            }
-            if(y1-1>=0){
-                if(map[x1][y1-1]==0){
-                    if(vis[x1][y1-1][x1][y1]==0){
-                        if(map[x2][y2-1]==0){
-                            result.add(new Robot(new Position(x1,y1-1),position1,num+1));
-                            vis[x1][y1-1][x1][y1] = vis[x1][y1][x2][y2]+1;
-                        }
-                    }
-                }
-            }
-            if(y2-1>=0){
-                if(map[x2][y2-1]==0){
-                    if(vis[x2][y2-1][x2][y2]==0){
-                        if(map[x1][y1-1]==0){
-                            result.add(new Robot(new Position(x2,y2-1),position2,num+1));
-                            vis[x2][y2-1][x2][y2]  = vis[x1][y1][x2][y2]+1;
-                        }
-                    }
-                }
-            }
-        }
-        return result;
-    }
+    public static int inf;
+    public static Queue<Node> q = new LinkedList<>();
 
     public int solution(int[][] board) {
         n = board.length;
-        map = board;
-        vis = new int[board.length][board.length][board.length][board.length];
-        Queue<Robot> q = new LinkedList<>();
-        q.offer(new Robot(new Position(0,0), new Position(0,1),0));
-        vis[0][0][0][1] = 1;
-        while(!q.isEmpty()){
-            Robot robot = q.poll();
-            if(robot.getPosition2().getX()==n-1&&robot.getPosition2().getY()==n-1) resultVal = Math.min(resultVal,robot.getNum());
-            ArrayList<Robot> moveReturn = move(robot);
-            for(int i=0;i<moveReturn.size();i++){
-                q.offer(moveReturn.get(i));
+        inf = n*n + 1;
+        vis = new int[2][n][n];
+        for(int i=0; i<2; i++){
+            for(int j=0; j<n; j++){
+                for(int k=0; k<n;k++){
+                    vis[i][j][k] = inf;
+                }
             }
         }
-        return resultVal;
+        q.offer(new Node(0,0,0));
+        vis[0][0][0] = 0;
+        while (!q.isEmpty()){
+            Node node = q.poll();
+            int dir = node.getDir();
+            int x = node.getX();
+            int y = node.getY();
+            int nextCnt = vis[dir][x][y] + 1;
+            if(dir==0){
+                if(y+2<n && board[x][y+2]==0 && vis[0][x][y+1]==inf){
+                    vis[0][x][y+1] = nextCnt;
+                    q.offer(new Node(0,x,y+1));
+                }
+                if(y-1>=0 && board[x][y-1]==0 && vis[0][x][y-1]==inf){
+                    vis[0][x][y-1] = nextCnt;
+                    q.offer(new Node(0,x,y-1));
+                }
+                if(x+1<n && board[x+1][y]==0 && board[x+1][y+1]==0 && vis[0][x+1][y]==inf){
+                    vis[0][x+1][y] = nextCnt;
+                    q.offer(new Node(0,x+1,y));
+                }
+                if(x-1>=0 && board[x-1][y]==0 && board[x-1][y+1]==0 && vis[0][x-1][y]==inf){
+                    vis[0][x-1][y] =  nextCnt;
+                    q.offer(new Node(0,x-1,y));
+                }
+                if(x+1<n && board[x+1][y]==0 && board[x+1][y+1]==0 && vis[1][x][y+1]==inf){
+                    vis[1][x][y+1] =  nextCnt;
+                    q.offer(new Node(1,x,y+1));
+                }
+                if(x+1<n && board[x+1][y]==0 && board[x+1][y+1]==0 && vis[1][x][y]==inf){
+                    vis[1][x][y] =  nextCnt;
+                    q.offer(new Node(1,x,y));
+                }
+                if(x-1>=0 && board[x-1][y]==0 && board[x-1][y+1]==0 && vis[1][x-1][y+1]==inf){
+                    vis[1][x-1][y+1] =  nextCnt;
+                    q.offer(new Node(1,x-1,y+1));
+                }
+                if(x-1>=0 && board[x-1][y]==0 && board[x-1][y+1]==0 && vis[1][x-1][y]==inf){
+                    vis[1][x-1][y] =  nextCnt;
+                    q.offer(new Node(1,x-1,y));
+                }
+            }
+            else{
+                if(x+2<n && board[x+2][y]==0 && vis[1][x+1][y]==inf){
+                    vis[1][x+1][y] = nextCnt;
+                    q.offer(new Node(1,x+1,y));
+                }
+                if(x-1>=0 && board[x-1][y]==0 && vis[1][x-1][y]==inf){
+                    vis[1][x-1][y] = nextCnt;
+                    q.offer(new Node(1,x-1,y));
+                }
+                if(y+1<n && board[x][y+1]==0 && board[x+1][y+1]==0 && vis[1][x][y+1]==inf){
+                    vis[1][x][y+1] = nextCnt;
+                    q.offer(new Node(1,x,y+1));
+                }
+                if(y-1>=0 && board[x][y-1]==0 && board[x+1][y-1]==0 && vis[1][x][y-1]==inf){
+                    vis[1][x][y-1] = nextCnt;
+                    q.offer(new Node(1,x,y-1));
+                }
+                if(y+1<n && board[x][y+1]==0 && board[x+1][y+1]==0 && vis[0][x][y]==inf){
+                    vis[0][x][y] = nextCnt;
+                    q.offer(new Node(0,x,y));
+                }
+                if(y+1<n && board[x][y+1]==0 && board[x+1][y+1]==0 && vis[0][x+1][y]==inf){
+                    vis[0][x+1][y] = nextCnt;
+                    q.offer(new Node(0,x+1,y));
+                }
+                if(y-1>=0 && board[x][y-1]==0 && board[x+1][y-1]==0 && vis[0][x][y-1]==inf){
+                    vis[0][x][y-1] = nextCnt;
+                    q.offer(new Node(0,x,y-1));
+                }
+                if(y-1>=0 && board[x][y-1]==0 && board[x+1][y-1]==0 && vis[0][x+1][y-1]==inf){
+                    vis[0][x+1][y-1] = nextCnt;
+                    q.offer(new Node(0,x+1,y-1));
+                }
+            }
+        }
+
+
+        int answer = Math.min(vis[0][n-1][n-1-1],vis[1][n-1-1][n-1]);
+        return answer;
     }
 }
