744b9b6e33eb38ebbdc2ccb1736d75cfd0027634
vividswan <vividswan@naver.com>
2021-04-21 00:11:01 +0900

    Update 괄호변환.java

--- Programmers/Level 2/괄호변환.java
+++ Programmers/Level 2/괄호변환.java
@@ -1,47 +1,56 @@
 import java.util.Stack;
 
 class Solution {
-
-    public static String recursionV(String str){
-        if(str.isEmpty()) return "";
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int maxIdx = 0;
-        boolean isRight = true;
-        for(int i=0;i<str.length();i++){
-            char nowC = str.charAt(i);
-            if(nowC=='(') cnt1 ++;
-            else cnt2++;
-            if(cnt1==cnt2 && maxIdx==0) maxIdx = i;
-        }
+    public static boolean isRightStr(String str){
         Stack<Character> s = new Stack<>();
-        for(int i=0; i<=maxIdx;i++){
-            if(str.charAt(i)=='(') s.push('(');
-            else{
-                if(!s.isEmpty()) s.pop();
-                else {
-                    isRight = false;
-                    break;
-                }
+        for(int i=0; i< str.length() ;i++){
+            char now = str.charAt(i);
+            if(now=='(') s.push(now);
+            else {
+                if(s.isEmpty()) return false;
+                else s.pop();
             }
         }
-        if(!s.isEmpty()) isRight = false;
-        if(isRight) {
-            return (str.substring(0,maxIdx+1) + recursionV(str.substring(maxIdx+1)));
-        }
-        else {
-            String now = "";
-            for(int i=1;i<maxIdx;i++){
-                if(str.charAt(i)=='(') now += ")";
-                else now += "(";
-            }
-            return ("("+recursionV(str.substring(maxIdx+1))+")" + now);
-        }
+        if(s.isEmpty()) return true;
+        else return false;
     }
-
+    public String recursion(String str){
+        if(isRightStr(str)) return str;
+        String temp = "";
+        String u = "";
+        String v = "";
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') left++;
+            else right++;
+            if (left == right) {
+                u = str.substring(0, i + 1);
+                v = str.substring(i + 1);
+                break;
+            }
+        }
+        if(isRightStr(u)){
+            temp += u;
+            if(!v.isEmpty()) {
+                temp += recursion(v);
+            }
+            return temp;
+        }
+        temp += "(";
+        temp += recursion(v);
+        temp += ")";
+        String lastTemp = "";
+        for(int i=1; i<u.length()-1 ; i++){
+            if(u.charAt(i)=='(') lastTemp += ')';
+            else lastTemp += '(';
+        }
+        temp += lastTemp;
+        return temp;
+    }
     public String solution(String p) {
         String answer = "";
-        answer += recursionV(p);
+        answer += recursion(p);
         return answer;
     }
 }
