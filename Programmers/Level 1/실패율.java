2b0ce17144af0475d46de8c72c5596bb9b083c05
vividswan <vividswan@naver.com>
2021-04-28 09:06:46 +0900

    Update 실패율.java

--- Programmers/Level 1/실패율.java
+++ Programmers/Level 1/실패율.java
@@ -1,61 +1,52 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
-class Stage implements Comparable<Stage>{
+class Node implements Comparable<Node>{
     private int idx;
-    private float proccess;
-
-    public Stage(int idx, float proccess){
-        this.idx = idx;
-        this.proccess = proccess;
-    }
+    private double val;
 
     public int getIdx() {
         return idx;
     }
 
+    public double getVal() {
+        return val;
+    }
+
+    public Node(int idx, double val){
+        this.idx = idx;
+        this.val = val;
+    }
+
     @Override
-    public int compareTo(Stage other) {
-        if(this.proccess != other.proccess)return Float.compare(other.proccess, this.proccess);
-        else return Integer.compare(this.idx, other.idx);
+    public int compareTo(Node other) {
+        return Double.compare(other.val,this.val);
     }
 }
 
 class Solution {
-
-    public static int[] cnt;
-    public static int[] notSuccess;
-    public static float[] res;
+    public static double[] clear;
+    public static double[] notClear;
 
     public int[] solution(int N, int[] stages) {
-
-        cnt = new int[N+2];
-        notSuccess = new int[N+2];
-        res = new float[N+2];
-
-        ArrayList<Stage> list = new ArrayList<>();
-
-        for(int i=0; i<stages.length;i++){
-            int stage = stages[i];
-            for(int j=1; j<=stage;j++){
-                cnt[j]++;
-            }
-            notSuccess[stage]++;
+        clear = new double[N+2];
+        notClear = new double[N+2];
+        for(int i=0; i<stages.length; i++){
+            int now = stages[i];
+            notClear[now]++;
+            for(int j=1; j<=now; j++) clear[j]++;
         }
-
-        for(int i=1;i<=N;i++){
-            if(cnt[i]==0) list.add(new Stage(i,0));
-            else list.add(new Stage(i, ((float)notSuccess[i]/cnt[i])));
+        ArrayList<Node> list = new ArrayList<>();
+        for(int i=1; i<=N; i++){
+            if(clear[i]==0) list.add(new Node(i,  0));
+            else list.add(new Node(i,  (notClear[i]/clear[i])));
         }
-
         Collections.sort(list);
-
         int[] answer = new int[N];
-
-        for(int i=0;i<list.size();i++){
+        for(int i=0; i<N;i++){
             answer[i] = list.get(i).getIdx();
         }
-
         return answer;
     }
 }
