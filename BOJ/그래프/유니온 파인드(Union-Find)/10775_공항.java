f0813af8edc65af0b5ae064b7801e78951ec7dfc
vividswan <vividswan@naver.com>
2021-04-29 09:13:07 +0900

    Update 10775_공항.java

--- BOJ/그래프/유니온 파인드(Union-Find)/10775_공항.java
+++ BOJ/그래프/유니온 파인드(Union-Find)/10775_공항.java
@@ -1,19 +1,28 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class 10775_공항 {
-    public static int g,p;
+public class Main {
+
+    public static int g, p;
     public static int[] parent;
+
     public static int find(int idx){
-        if(parent[idx]==idx) return idx;
+        if(idx==parent[idx]) return idx;
         else return parent[idx] = find(parent[idx]);
     }
-    public static void merge(int x, int y){
-        x = find(x);
-        y= find(y);
-        if(x==y) return;
-        if(x < y) parent[y] = x;
-        else parent[x] = y;
+
+    public static void merge(int a, int b){
+        a= find(a);
+        b = find(b);
+        if(a==b) return;
+
+        if(a > b){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        parent[b] = a;
     }
 
     public static void main(String[] args) throws IOException {
@@ -22,21 +31,26 @@ public class 10775_공항 {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         g = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
         p = Integer.parseInt(st.nextToken());
 
         parent = new int[g+1];
-        for(int i=1;i<=g;i++) parent[i] = i;
-        int res = 0;
-        for(int i=1; i<=p;i++){
+        for(int i=0; i<=g; i++) parent[i] = i;
+
+        int cnt = 0;
+        boolean isEnd = false;
+        for(int i=1; i<=p; i++){
             st = new StringTokenizer(br.readLine());
-            int val = Integer.parseInt(st.nextToken());
-            int nowParent = find(val);
-            if(nowParent==0) break;
-            merge(nowParent,nowParent-1);
-            res = i;
+            int now = Integer.parseInt(st.nextToken());
+            int rootNode = find(now);
+            if(rootNode==0) isEnd = true;
+            if(!isEnd){
+                cnt++;
+                merge(rootNode-1, rootNode);
+            }
         }
-        bw.write(String.valueOf(res)+"\n");
+        bw.write(String.valueOf(cnt)+"\n");
         bw.flush();
         br.close();
         bw.close();
