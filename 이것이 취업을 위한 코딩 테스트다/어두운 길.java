2b9fc08b9a50c80e3eaf6ddf15a57bd58df7431e
vividswan <vividswan@naver.com>
2021-05-03 10:10:11 +0900

    Update 어두운 길.java

--- 이것이 취업을 위한 코딩 테스트다/어두운 길.java
+++ 이것이 취업을 위한 코딩 테스트다/어두운 길.java
@@ -1,88 +1,99 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Node implements Comparable<Node>{
+class Connection implements Comparable<Connection>{
     private int x;
     private int y;
     private int value;
 
-    public int getX() {
-        return x;
+    public Connection(int x, int y, int value){
+        this.x = x;
+        this.y = y;
+        this.value = value;
     }
 
     public int getY() {
         return y;
     }
 
+    public int getX() {
+        return x;
+    }
+
     public int getValue() {
         return value;
     }
 
-    public Node(int x, int y, int value){
-        this.x = x;
-        this.y = y;
-        this.value = value;
-    }
-
     @Override
-    public int compareTo(Node other) {
-        return Integer.compare(this.value, other.value);
+    public int compareTo(Connection other) {
+        return Integer.compare(this.value,other.value);
     }
 }
 
-public class 어두운 길 {
+public class Main {
 
-    public static int n,m,sum,minSum;
+    public static int n,m,tot;
     public static int[] parent;
+
     public static int find(int idx){
-        if(idx==parent[idx]) return idx;
-        else return parent[idx]= find(parent[idx]);
+        if(parent[idx]==idx) return idx;
+        else return parent[idx] =find(parent[idx]);
     }
-    public static void merge(int x, int y){
-        x = find(x);
-        y = find(y);
-        if(x==y) return;
-        if(x<y) parent[y] = x;
-        else parent[x] = y;
+
+    public static void merge(int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if(a==b) return;
+        if(a>b){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        parent[b] = a;
     }
-    public static ArrayList<Node> arr = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+
         parent = new int[n];
+
         for(int i=0; i<n; i++) parent[i] = i;
+
+        PriorityQueue<Connection> pq = new PriorityQueue<>();
+
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
-            int x,y,val;
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            val = Integer.parseInt(st.nextToken());
-            sum+=val;
-            arr.add(new Node(x,y,val));
-        }
-        Collections.sort(arr);
-        for(int i=0;i<m;i++){
-            Node node = arr.get(i);
-            int x = node.getX();
-            int y = node.getY();
-            int value = node.getValue();
 
-            if(find(x)==find(y)) continue;
-            else {
-                merge(x,y);
-                minSum += value;
-            }
+            int x = Integer.parseInt(st.nextToken());
+            int y =Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+            pq.offer(new Connection(x,y,value));
         }
-        bw.write(String.valueOf(sum-minSum));
+        while(!pq.isEmpty()){
+            Connection connection = pq.poll();
+            int x = connection.getX();
+            int y = connection.getY();
+            int value = connection.getValue();
+            if(find(x)==find(y)){
+                tot += value;
+                continue;
+            }
+            else merge(x,y);
+        }
+        bw.write(String.valueOf(tot)+"\n");
         bw.flush();
         br.close();
         bw.close();
+
     }
 }
