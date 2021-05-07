972de9978d980d3be4250fb7bdea3cef1472d016
vividswan <vividswan@naver.com>
2021-05-07 10:08:12 +0900

    Update 18353_병사 배치하기.java

--- BOJ/동적 계획법(DP)/18353_병사 배치하기.java
+++ BOJ/동적 계획법(DP)/18353_병사 배치하기.java
@@ -1,34 +1,41 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class 18353_병사 배치하기 {
-    public static int[] arr;
+public class Main {
+
+    public static int n, res;
+    public static int[] val;
     public static int[] dp;
-    public static int n;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
-        arr = new int[n+1];
-        dp = new int[n+1];
+        val = new int[n];
+        dp = new int[n];
+
         st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=n;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n; i++){
+            val[i] = Integer.parseInt(st.nextToken());
             dp[i] = 1;
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<i;j++){
-                if(arr[i]<arr[j]) dp[i] = Math.max(dp[i],dp[j]+1);
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(val[i] > val[j]){
+                    dp[j] = Math.max(dp[j],dp[i]+1);
+                }
             }
         }
-        int maxVal = 0;
-        for(int i = 1; i<=n;i++) maxVal = Math.max(maxVal,dp[i]);
-        int result = n - maxVal;
-        bw.write(String.valueOf(result)+"\n");
+        for(int i=0; i<n; i++) res = Math.max(res,dp[i]);
+
+        bw.write(String.valueOf(n - res)+"\n");
         bw.flush();
         br.close();
         bw.close();
+
     }
 }
