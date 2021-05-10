536333699b7afca6e18e836769517385b1e6be3d
vividswan <vividswan@naver.com>
2021-05-10 10:47:35 +0900

    Update 정확한 순위.java

--- 이것이 취업을 위한 코딩 테스트다/정확한 순위.java
+++ 이것이 취업을 위한 코딩 테스트다/정확한 순위.java
@@ -1,8 +1,9 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class 정확한 순위 {
-    public static int n,m;
+public class Main {
+
+    public static int n, m;
     public static int[][] map;
     public static final int INF = (int)1e9;
 
@@ -11,42 +12,41 @@ public class 정확한 순위 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n+1][n+1];
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<=n; i++){
             for(int j=1; j<=n; j++){
-                if(i != j) map[i][j] = INF;
+                if(i!=j) map[i][j] = INF;
             }
         }
-
-        for(int i=1;i<=m;i++){
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            map[x][y] = 1;
-            map[y][x] = -1;
+        for(int i=1; i<=m; i++){
+            st= new StringTokenizer(br.readLine());
+            int go = Integer.parseInt(st.nextToken());
+            int ed = Integer.parseInt(st.nextToken());
+            map[go][ed] = 1;
         }
-        for(int k=1; k<=n;k++){
+        for(int k=1; k<=n; k++){
             for(int i=1; i<=n; i++){
                 for(int j=1; j<=n; j++){
-                    if(map[i][j]!=INF) continue;
-                    if(map[i][k]==1 && map[k][j]==1) map[i][j]=1;
-                    if(map[i][k]==-1 && map[k][j]==-1) map[i][j]=-1;
+                    if(map[i][j] > map[i][k]+map[k][j]){
+                        map[i][j] = map[i][k]+map[k][j];
+                    }
                 }
             }
         }
         int cnt = 0;
-        for(int i=1;i<=n;i++){
-            boolean isAns = true;
+        for(int i=1; i<=n; i++){
+            int flagCnt= 0;
             for(int j=1; j<=n; j++){
-                if(i==j) continue;
-                if(map[i][j]==INF) isAns=false;
+                if(map[i][j]==INF && map[j][i]==INF) break;
+                flagCnt++;
             }
-            if(isAns) cnt++;
+            if(flagCnt==n) cnt++;
         }
-        bw.write(String.valueOf(cnt));
+        bw.write(String.valueOf(cnt)+"\n");
         bw.flush();
         br.close();
         bw.close();
