ae406e2520a460e19c90bbdea283c050dbefad1a
vividswan <vividswan@naver.com>
2021-05-11 00:50:31 +0900

    Update 2110_공유기 설치.java

--- BOJ/탐색/이분 탐색(Binary Search)/2110_공유기 설치.java
+++ BOJ/탐색/이분 탐색(Binary Search)/2110_공유기 설치.java
@@ -1,50 +1,59 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import javax.management.StringValueExp;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class 2110_공유기 설치 {
+public  class Main  {
+
     public static int n,c;
     public static int[] arr;
-    public static int ans;
-    public static void getAns(){
-        int le = 0;
-        int ri = arr[n-1] - arr[0];
-        while(le <= ri){
-            int mid = (ri+le)/2;
-            int count =1;
-            int now =  0;
-            for(int i=1; i<n;i++){
-                if(arr[i]-arr[now]>=mid){
-                    now = i;
-                    count++;
-                }
+
+    public static boolean isPossible(int dist){
+        int nowIdx = 0;
+        int nextDist = arr[nowIdx] + dist;
+        int cnt = c-1;
+        for(int i=1; i<n; i++){
+            if(cnt == 0) break;
+            if(arr[i] < nextDist) continue;
+            else {
+                cnt--;
+                nowIdx = i;
+                nextDist = arr[nowIdx] + dist;
             }
-            if(count>=c) ans = mid;
-            if(count < c) ri = mid-1;
-            else le = mid+1;
         }
+        if(cnt==0) return true;
+        else return false;
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
+
         arr = new int[n];
 
-        for(int i=0;i<n;i++){
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-
-        getAns();
-        bw.write(String.valueOf(ans));
+        int left = 0;
+        int right = arr[n-1];
+        int ans = 0;
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(isPossible(mid)) {
+                ans = mid;
+                left = mid+1;
+            }
+            else right = mid-1;
+        }
+        bw.write(String.valueOf(ans)+"\n");
         bw.flush();
         br.close();
         bw.close();
