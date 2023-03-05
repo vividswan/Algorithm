130db881a7f282cd86115ce58f953a3db060edf2
Soohwan Park <vividswan@naver.com>
2023-03-05 14:26:33 +0900

    Update 2231_분해합.java

--- BOJ/탐색/완전 탐색(Brute-force)/2231_분해합.java
+++ BOJ/탐색/완전 탐색(Brute-force)/2231_분해합.java
@@ -3,34 +3,30 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StreamTokenizer;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static int getDecompositionSum(int value){
-		int sum = value;
-		String valueString = String.valueOf(value);
-		for (int i = 0; i < valueString.length(); i++) {
-			char now = valueString.charAt(i);
-			sum += now - '0';
-		}
-		return sum;
-	}
-
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int num = Integer.parseInt(st.nextToken());
-		int ans = 0;
-		for (int i = 1; i <= num; i++) {
-			if (getDecompositionSum(i) == num) {
-				ans = i;
+		int targetNumber = Integer.parseInt(st.nextToken());
+		int result = 0;
+		for (int i = 0; i < targetNumber; i++) {
+			int tempNumber = i;
+			int nowTarget = i;
+			while (tempNumber > 0) {
+				nowTarget += tempNumber % 10;
+				tempNumber = tempNumber / 10;
+			}
+			if (nowTarget == targetNumber) {
+				result = i;
 				break;
 			}
 		}
-		bw.write(ans+"\n");
-		bw.flush();
-		br.close();
+		bw.write(result+"\n");
 		bw.close();
+		br.close();
 	}
 }
