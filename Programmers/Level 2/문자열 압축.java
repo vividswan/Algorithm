c27ca3f420cb5b069921b14669751a69ec757afa
Soohwan Park <vividswan@naver.com>
2023-07-30 00:24:32 +0900

    Update 문자열 압축.java

--- Programmers/Level 2/문자열 압축.java
+++ Programmers/Level 2/문자열 압축.java
@@ -1,29 +1,34 @@
 class Solution {
-    public int solution(String s) {
-        int answer = s.length();
-        for(int i=1;i<s.length()/2+1;i++){
-            int step = i;
-            String prev = s.substring(0,step);
-            String resultString = "";
-            int cnt = 1;
-            for(int j=i;j<s.length();j+=step){
-                String nowPrev = "";
-                for(int k=j;k<j+step;k++){
-                    if(k<s.length())nowPrev += s.charAt(k);
-                }
-                if(prev.equals(nowPrev)) cnt++;
-                else {
-                    if(cnt>1) resultString += cnt+prev;
-                    else resultString += prev;
-                    cnt = 1;
-                    prev=nowPrev;
-                }
-            }
-            if(cnt>1) resultString += cnt+prev;
-            else resultString += prev;
-            answer= Math.min(answer,resultString.length());
-        }
-
-        return answer;
-    }
+	public int solution(String string) {
+		int answer = string.length();
+		for (int i = 1; i < string.length(); i++) {
+			int count = 0;
+			String nowString = "";
+			int nowValue = 1;
+			StringBuilder sb = new StringBuilder();
+			while (true) {
+				String targetString = string.substring(i * count, Math.min(i * (count + 1), string.length()));
+				if (targetString.equals(nowString)) {
+					nowValue++;
+				} else {
+					if (nowValue != 1) {
+						sb.append(nowValue);
+					}
+					sb.append(nowString);
+					nowString = targetString;
+					nowValue = 1;
+				}
+				count++;
+				if (i * count > string.length()) {
+					if (nowValue != 1) {
+						sb.append(nowValue);
+					}
+					sb.append(nowString);
+					break;
+				}
+			}
+			answer = Math.min(sb.length(), answer);
+		}
+		return answer;
+	}
 }
